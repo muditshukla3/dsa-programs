1177af93733cb63d45184c2ea1faad38d513abb2
Mudit Shukla <mudit@coffeebeans.io>
2023-01-21 20:57:30 +0530

    Added array problems

---
+++ src/main/java/com/ms/hard/arrays/MinimumDeliveryBoys.java
@@ -0,0 +1,21 @@
+package com.ms.hard.arrays;
+
+/**
+ * A newspaper vendor wants to employ delivery boys. There are N houses in the locality arranged in one dimensional
+ * lanes present at 1,2,3...N. There are M delivery boys. The ith delivery boy can deliver to house from ai to bi.
+ * Given 1st input as number of houses, 2nd input as number of delivery boys and rd input a two-dimensional array
+ * representing each delivery boy's delivery range, find out the minimum number of delivery boys newspaper vendor
+ * needs to cover all house.
+ *
+ * Example: noOfHouse- 6, noOfBoys- 4, range: [{1,2},{2,4},{1,4},{5,6}]
+ * Output - 2
+ *
+ * Example: noOfHouse- 5, noOfBoys- 2, range: [{1,4},{1,3}]
+ * Output: -1
+ */
+public class MinimumDeliveryBoys {
+
+    public int minimumBoysNeeded(int noOfHouse, int noOfBoys, int[][] range){
+        return -1;
+    }
+}
---
+++ src/main/java/com/ms/medium/arrays/MaxChunks.java
@@ -0,0 +1,37 @@
+package com.ms.medium.arrays;
+
+import java.util.Arrays;
+
+/**
+ *  https://leetcode.com/problems/max-chunks-to-make-sorted/
+ *  You are given an integer array arr of length n that represents a permutation of the integers
+ *  in the range [0, n - 1]. We split arr into some number of chunks (i.e., partitions), and
+ *  individually sort each chunk. After concatenating them, the result should equal the sorted array.
+ *  Return the largest number of chunks we can make to sort the array.
+ */
+public class MaxChunks {
+
+    public int maxChunksToSorted(int[] array){
+        int n = array.length;
+        int[] minOfRight = new int[n];
+        minOfRight[n-1] = array[n-1];
+
+        for (int i=n-2;i>0;i--){
+            minOfRight[i] = Math.min(minOfRight[i+1], array[i]);
+        }
+        Arrays.stream(minOfRight).forEach(System.out::print);
+        int result=0;
+        int max = Integer.MIN_VALUE;
+        for (int i=0;i<n-1;i++){
+            max = Math.max(max, array[i]);
+            if(max <= minOfRight[i+1]){
+                result++;
+            }
+        }
+        return result+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MaxChunks().maxChunksToSorted(new int[]{2,4,1,6,5,9,7}));
+    }
+}
