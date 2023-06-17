b402a3da74da6a899d671f47d4d583a0518c9f4c
muditshukla3 <muditshukla3@gmail.com>
2023-06-17 11:25:18 +0530

    Added implementation using deque

--- src/main/java/com/ms/easy/ExpressionCheck.java
+++ src/main/java/com/ms/easy/ExpressionCheck.java
@@ -1,8 +1,6 @@
 package com.ms.easy;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Given a string s containing just the characters
@@ -34,6 +32,31 @@ public class ExpressionCheck {
         }
 
         return stack.empty();
+    }
+
+    public static boolean isValid(String str) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(int i = 0; i<str.length(); i++) {
+            if(!map.containsKey((str.charAt(i)))){
+                stack.offerFirst(str.charAt(i));
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }else {
+                    if(stack.peekFirst() == map.get(str.charAt(i))){
+                        stack.pollFirst();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 
 }
